763c37694d5799bfb98f374e259b5a9b272501dd
Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
2023-02-09 11:50:00 -0500

    Add tests

--- tests/unit/TestDataWarehouse.py
+++ tests/unit/TestDataWarehouse.py
@@ -1,9 +1,12 @@
-import unittest
 import os
+import pandas
+import unittest
 import xdmod.datawarehouse as xdw
 
 
 class TestDataWarehouse(unittest.TestCase):
+    __INVALID_STR = 'asdlkfjsdlkfisdjkfjd'
+    __VALID_REALM = 'Jobs'
     __XDMOD_URL = 'https://xdmod-dev.ccr.xdmod.org'
 
     def setUp(self):
@@ -25,7 +28,7 @@ class TestDataWarehouse(unittest.TestCase):
     def test___init___KeyError_XDMOD_PASS(self):
         old_environ = dict(os.environ)
         os.environ.clear()
-        os.environ['XDMOD_USER'] = 'sdfjlksdf'
+        os.environ['XDMOD_USER'] = self.__INVALID_STR
         try:
             with self.assertRaises(KeyError):
                 xdw.DataWarehouse(self.__XDMOD_URL)
@@ -61,6 +64,10 @@ class TestDataWarehouse(unittest.TestCase):
             with xdw.DataWarehouse('asdklsdfj://sdlkfs'):
                 pass
 
+    def test_get_realms_return_type(self):
+        with self.__valid_dw:
+            self.assertIsInstance(self.__valid_dw.get_realms(), tuple)
+
     def test_get_realms_RuntimeError_outside_context(self):
         with self.assertRaises(RuntimeError):
             self.__valid_dw.get_realms()
@@ -68,7 +75,12 @@ class TestDataWarehouse(unittest.TestCase):
     def test_get_metrics_KeyError(self):
         with self.__valid_dw:
             with self.assertRaises(KeyError):
-                self.__valid_dw.get_realms('asdfasd')
+                self.__valid_dw.get_metrics(self.__INVALID_STR)
+
+    def test_get_metrics_return_type(self):
+        with self.__valid_dw:
+            self.assertIsInstance(self.__valid_dw.get_metrics(self.__VALID_REALM),
+                                                              pandas.core.frame.DataFrame)
 
     def test_get_metrics_TypeError(self):
         with self.__valid_dw:
@@ -77,42 +89,42 @@ class TestDataWarehouse(unittest.TestCase):
 
     def test_get_metrics_RuntimeError_outside_context(self):
         with self.assertRaises(RuntimeError):
-            self.__valid_dw.get_metrics()
+            self.__valid_dw.get_metrics(self.__VALID_REALM)
 
     def test_get_dataset_KeyError_duration(self):
         with self.__valid_dw:
             with self.assertRaises(KeyError):
-                self.__valid_dw.get_dataset(duration='asdf')
+                self.__valid_dw.get_dataset(duration=self.__INVALID_STR)
 
     def test_get_dataset_KeyError_realm(self):
         with self.__valid_dw:
             with self.assertRaises(KeyError):
-                self.__valid_dw.get_dataset(realm='asdf')
+                self.__valid_dw.get_dataset(realm=self.__INVALID_STR)
 
     def test_get_dataset_KeyError_metric(self):
         with self.__valid_dw:
             with self.assertRaises(KeyError):
-                self.__valid_dw.get_dataset(metric='asdf')
+                self.__valid_dw.get_dataset(metric=self.__INVALID_STR)
 
     def test_get_dataset_KeyError_dimension(self):
         with self.__valid_dw:
             with self.assertRaises(KeyError):
-                self.__valid_dw.get_dataset(dimension='asdf')
+                self.__valid_dw.get_dataset(dimension=self.__INVALID_STR)
 
     def test_get_dataset_KeyError_filters(self):
         with self.__valid_dw:
             with self.assertRaises(KeyError):
-                self.__valid_dw.get_dataset(filters={'asdf'})
+                self.__valid_dw.get_dataset(filters={self.__INVALID_STR})
 
     def test_get_dataset_KeyError_dataset_type(self):
         with self.__valid_dw:
             with self.assertRaises(KeyError):
-                self.__valid_dw.get_dataset(dataset_type='asdf')
+                self.__valid_dw.get_dataset(dataset_type=self.__INVALID_STR)
 
     def test_get_dataset_KeyError_aggregation_unit(self):
         with self.__valid_dw:
             with self.assertRaises(KeyError):
-                self.__valid_dw.get_dataset(aggregation_unit='asdf')
+                self.__valid_dw.get_dataset(aggregation_unit=self.__INVALID_STR)
 
     def test_get_dataset_RuntimeError_outside_context(self):
         with self.assertRaises(RuntimeError):
@@ -161,12 +173,14 @@ class TestDataWarehouse(unittest.TestCase):
     def test_get_dataset_RuntimeError_start_date_malformed(self):
         with self.__valid_dw:
             with self.assertRaises(RuntimeError):
-                self.__valid_dw.get_dataset(duration=('asdf', '2022-01-01'))
+                self.__valid_dw.get_dataset(duration=(self.__INVALID_STR,
+                                                      '2022-01-01'))
 
     def test_get_dataset_RuntimeError_end_date_malformed(self):
         with self.__valid_dw:
             with self.assertRaises(RuntimeError):
-                self.__valid_dw.get_dataset(duration=('2022-01-01', 'asdf'))
+                self.__valid_dw.get_dataset(duration=('2022-01-01',
+                                                      self.__INVALID_STR))
 
 
 if __name__ == '__main__':
