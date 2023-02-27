33e172e31a62a9549c8b096c19bad1b18dc06d04
Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
2023-02-26 22:06:01 -0500

    Speed up functional tests
    by only using one runtime context. Also, the tests don't need to be grouped into a class.

--- tests/functional/test_datawarehouse_functional.py
+++ tests/functional/test_datawarehouse_functional.py
@@ -1,46 +1,49 @@
-import os
-import pandas
 import pytest
 import xdmod.datawarehouse as xdw
+import pandas
+import os
+
+XDMOD_URL = 'https://xdmod-dev.ccr.xdmod.org'
+DATA_DIR = os.path.dirname(__file__) + '/data'
 
 
-class TestDataWarehouse:
-    __DATA_DIR = os.path.dirname(__file__) + '/data'
-    __XDMOD_URL = 'https://xdmod-dev.ccr.xdmod.org'
+@pytest.fixture(scope='module')
+def valid_dw():
+    with xdw.DataWarehouse(XDMOD_URL) as dw:
+        yield dw
 
-    @pytest.fixture
-    def valid_dw(self):
-        yield xdw.DataWarehouse(self.__XDMOD_URL)
 
-    def __assert_dfs_equal(self, data_file, actual):
-        expected = pandas.read_csv(
-            self.__DATA_DIR + '/' + data_file, dtype='object'
-        )
-        expected = expected.set_index('id')
-        assert expected.equals(actual)
+def __assert_dfs_equal(data_file, actual):
+    expected = pandas.read_csv(
+        DATA_DIR + '/' + data_file, dtype='object'
+    )
+    expected = expected.set_index('id')
+    assert expected.equals(actual)
 
-    def test_get_realms(self, valid_dw):
-        with valid_dw:
-            self.__assert_dfs_equal(
-                'xdmod-dev-realms.csv', valid_dw.get_realms()
-            )
 
-    def test_get_metrics(self, valid_dw):
-        with valid_dw:
-            self.__assert_dfs_equal(
-                'xdmod-dev-jobs-metrics.csv', valid_dw.get_metrics('Jobs')
-            )
+def test_get_realms(valid_dw):
+    __assert_dfs_equal(
+        'xdmod-dev-realms.csv',
+        valid_dw.get_realms()
+    )
 
-    def test_get_dimensions(self, valid_dw):
-        with valid_dw:
-            self.__assert_dfs_equal(
-                'xdmod-dev-jobs-dimensions.csv',
-                valid_dw.get_dimensions('Jobs')
-            )
 
-    def test_get_filters(self, valid_dw):
-        with valid_dw:
-            self.__assert_dfs_equal(
-                'xdmod-dev-jobs-fieldofscience-filters.csv',
-                valid_dw.get_filters('Jobs', 'Field of Science')
-            )
+def test_get_metrics(valid_dw):
+    __assert_dfs_equal(
+        'xdmod-dev-jobs-metrics.csv',
+        valid_dw.get_metrics('Jobs')
+    )
+
+
+def test_get_dimensions(valid_dw):
+    __assert_dfs_equal(
+        'xdmod-dev-jobs-dimensions.csv',
+        valid_dw.get_dimensions('Jobs')
+    )
+
+
+def test_get_filters(valid_dw):
+    __assert_dfs_equal(
+        'xdmod-dev-jobs-fieldofscience-filters.csv',
+        valid_dw.get_filters('Jobs', 'Field of Science')
+    )
