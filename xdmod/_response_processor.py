f6e605c34760595f9544638e33cd045768c73802
Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
2023-03-08 17:24:07 -0500

    Rename variables...
    to use consistent naming style, be more accurate to what they are, and not use in-built
    function names.

--- xdmod/_response_processor.py
+++ xdmod/_response_processor.py
@@ -7,35 +7,35 @@ import re
 
 
 def _process_get_data_response(dw, params, response):
-    csvdata = csv.reader(response.splitlines())
+    csv_data = csv.reader(response.splitlines())
     if not params['timeseries']:
-        return __xdmod_csv_to_pandas(csvdata)
+        return __xdmod_csv_to_pandas(csv_data)
     else:
-        labelre = re.compile(r'\[([^\]]+)\].*')
-        timestamps = []
+        label_re = re.compile(r'\[([^\]]+)\].*')
+        time_values = []
         data = []
-        for line_num, line in enumerate(csvdata):
+        for line_num, line in enumerate(csv_data):
             if line_num == 5:
                 start_date, end_date = line
             elif line_num == 7:
-                dimensions = []
+                dimension_values = []
                 for label in line[1:]:
-                    match = labelre.match(label)
+                    match = label_re.match(label)
                     if match:
-                        dimensions.append(html.unescape(match.group(1)))
+                        dimension_values.append(html.unescape(match.group(1)))
                     else:
-                        dimensions.append(html.unescape(label))
+                        dimension_values.append(html.unescape(label))
             elif line_num > 7 and len(line) > 1:
                 date_string = line[0]
                 # Match YYYY-MM-DD
                 if re.match(r'^[0-9]{4}-[0-9]{2}-[0-9]{2}$', line[0]):
-                    format = '%Y-%m-%d'
+                    format_ = '%Y-%m-%d'
                 # Match YYYY-MM
                 elif re.match(r'^[0-9]{4}-[0-9]{2}$', line[0]):
-                    format = '%Y-%m'
+                    format_ = '%Y-%m'
                 # Match YYYY
                 elif re.match(r'^[0-9]{4}$', line[0]):
-                    format = '%Y'
+                    format_ = '%Y'
                 # Match YYYY Q#
                 elif re.match(r'^[0-9]{4} Q[0-9]$', line[0]):
                     year, quarter = line[0].split(' ')
@@ -53,18 +53,18 @@ def _process_get_data_response(dw, params, response):
                             + line[0] + '.'
                         )
                     date_string = year + '-' + month + '-01'
-                    format = '%Y-%m-%d'
+                    format_ = '%Y-%m-%d'
                 else:
                     raise Exception(
                         'Unsupported date specification ' + line[0] + '.'
                     )
-                timestamps.append(datetime.strptime(date_string, format))
+                time_values.append(datetime.strptime(date_string, format_))
                 data.append(np.asarray(line[1:], dtype=np.float64))
         return pd.DataFrame(
             data=data,
-            index=pd.Series(data=timestamps, name='Time'),
+            index=pd.Series(data=time_values, name='Time'),
             columns=pd.Series(
-                dimensions,
+                dimension_values,
                 name=dw._get_dimension_label(
                     params['realm'], params['dimension']
                 )
@@ -73,7 +73,7 @@ def _process_get_data_response(dw, params, response):
 
 
 def __xdmod_csv_to_pandas(csv_data):
-    groups = []
+    dimension_values = []
     data = []
     for line_num, line in enumerate(csv_data):
         if line_num == 5:
@@ -81,13 +81,13 @@ def __xdmod_csv_to_pandas(csv_data):
         elif line_num == 7:
             dimension, metric = line
         elif line_num > 7 and len(line) > 1:
-            groups.append(html.unescape(line[0]))
+            dimension_values.append(html.unescape(line[0]))
             data.append(np.float64(line[1]))
     if len(data) == 0:
         return pd.Series(dtype=np.float64)
     return pd.Series(
         data=data,
         name=metric,
-        index=pd.Series(data=groups, name=dimension),
+        index=pd.Series(data=dimension_values, name=dimension),
         dtype=np.float64
     )
