b6b47a5edc3cd7e50f36bd41abc9ce36b084a565
Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
2023-04-10 17:53:14 -0400

    Provide placeholder token for unit tests.

--- tests/unit/test_datawarehouse_unit.py
+++ tests/unit/test_datawarehouse_unit.py
@@ -7,6 +7,15 @@ VALID_XDMOD_URL = 'https://xdmod.access-ci.org'
 INVALID_STR = 'asdlkfjsdlkfisdjkfjd'
 
 
+@pytest.fixture(scope='module', autouse=True)
+def set_environ():
+    token = os.environ['XDMOD_API_TOKEN'] if 'XDMOD_API_TOKEN' in os.environ else ''
+    os.environ['XDMOD_API_TOKEN'] = INVALID_STR
+    yield
+    os.environ['XDMOD_API_TOKEN'] = token
+
+
+
 def test___init___TypeError_xdmod_host():
     with pytest.raises(TypeError, match='`xdmod_host` must be a string.'):
         xdw.DataWarehouse(2)
