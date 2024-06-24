e0727294ff4d2da0fb28b1a93310eb1c533b212c
RodneyCodess <160431998+RodneyCodess@users.noreply.github.com>
2024-06-24 11:27:50 -0400

    Update tests/regression/test_datawarehouse_regression.py
    Co-authored-by: Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>

--- tests/regression/test_datawarehouse_regression.py
+++ tests/regression/test_datawarehouse_regression.py
@@ -121,8 +121,8 @@ def test_get_data_filter_user(valid_dw):
 @pytest.mark.parametrize(
     'duration,aggregation_unit,data_file',
     [
-        (('2023-01-01', '2023-12-31'), 'Quarter', "jobs-2023-quarters.csv"),
-        (('2022-01-01', '2023-12-31'), 'Year', "jobs-2022-2023-years.csv"),
+        (('2023-01-01', '2023-12-31'), 'Quarter', 'jobs-2023-quarters.csv'),
+        (('2022-01-01', '2023-12-31'), 'Year', 'jobs-2022-2023-years.csv'),
     ],
     ids=('quarter', 'year'),
 )
