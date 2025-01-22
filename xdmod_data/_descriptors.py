9842f48fa18409bdb9fd760061a558d22391ff86
Aaron Weeden <aaronwee@buffalo.edu>
2025-01-22 13:53:34 -0500

    Implement 100% test coverage.

--- xdmod_data/_descriptors.py
+++ xdmod_data/_descriptors.py
@@ -19,7 +19,7 @@ class _Descriptors:
             '/controllers/metric_explorer.php',
             {'operation': 'get_dw_descripter'},
         )
-        if response['totalCount'] != 1:
+        if response['totalCount'] != 1:  # pragma: no cover
             raise RuntimeError(
                 'Descriptor received with unexpected structure.'
             )
