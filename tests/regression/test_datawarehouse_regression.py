bcfae8092f80896c149b5d47a7f42d1a44a3c34c
Aaron Weeden <aaronwee@buffalo.edu>
2025-01-23 10:04:09 -0500

    Set up Docker Compose for CI testing.
    Co-authored-by: Rodney Garnett <Garnettr35@gmail.com>

--- tests/regression/test_datawarehouse_regression.py
+++ tests/regression/test_datawarehouse_regression.py
@@ -1,23 +1,24 @@
 from datetime import date
 from dotenv import load_dotenv
 import numpy
-from os.path import dirname, expanduser
+import os
 import pandas
 from pathlib import Path
 import pytest
 from xdmod_data.warehouse import DataWarehouse
 
-XDMOD_URL = 'https://xdmod.access-ci.org'
+XDMOD_HOST = os.environ['XDMOD_HOST']
+XDMOD_VERSION = os.environ['XDMOD_VERSION']
 TOKEN_PATH = '~/.xdmod-data-token'
-DATA_DIR = dirname(__file__) + '/data'
+DATA_DIR = os.path.dirname(__file__) + '/data/' + XDMOD_VERSION
 
 
-load_dotenv(Path(expanduser(TOKEN_PATH)), override=True)
+load_dotenv(Path(os.path.expanduser(TOKEN_PATH)), override=True)
 
 
 @pytest.fixture(scope='module')
 def valid_dw():
-    with DataWarehouse(XDMOD_URL) as dw:
+    with DataWarehouse(XDMOD_HOST) as dw:
         yield dw
 
 
@@ -28,50 +29,75 @@ def __assert_dfs_equal(
     index_col='id',
     columns_name=None,
 ):
-    expected = pandas.read_csv(
-        DATA_DIR + '/' + data_file,
-        dtype=dtype,
-        index_col=index_col,
-        keep_default_na=False,
-        na_values=[''],
-    ).fillna(numpy.nan)
-    expected.columns = expected.columns.astype('string')
-    expected.columns.name = columns_name
-    if index_col == 'Time':
-        expected.index = pandas.to_datetime(expected.index)
-    assert expected.equals(actual)
+    if 'GENERATE_DATA_FILES' in os.environ:  # pragma: no cover
+        try:
+            os.mkdir(DATA_DIR)
+        except FileExistsError:
+            pass
+        actual.to_csv(DATA_DIR + '/' + data_file)
+    else:
+        expected = pandas.read_csv(
+            DATA_DIR + '/' + data_file,
+            dtype=dtype,
+            index_col=index_col,
+            keep_default_na=False,
+            na_values=[''],
+        ).fillna(numpy.nan)
+        expected.columns = expected.columns.astype('string')
+        expected.columns.name = columns_name
+        if index_col == 'Time':
+            expected.index = pandas.to_datetime(expected.index)
+        assert expected.equals(actual)
 
 
-def test_get_raw_data(valid_dw, capsys):
-    data = valid_dw.get_raw_data(
-        duration=('2023-05-01', '2023-05-02'),
-        realm='SUPREMM',
-        fields=(
-            'CPU User',
-            'Nodes',
-            'Wall Time',
-            'Wait Time',
-            'Requested Wall Time',
-            'Total memory used',
-            'Mount point "home" data written',
-            'Mount point "scratch" data written',
+@pytest.mark.parametrize(
+    'additional_params, number, csv_title',
+    [
+        (
+            {},
+            '54747',
+            'raw-data-every-1000-no-fields-no-filters.csv',
         ),
-        filters={
-            'Resource': [
-                'STAMPEDE2 TACC',
-                'Bridges 2 RM',
-            ],
-        },
-        show_progress=True,
-    ).iloc[::1000]
+        (
+            {
+                'fields':
+                (
+                    'Local Job Id',
+                    'Quality of Service',
+                    'GPUs',
+                    'Start Time',
+                    'Department',
+                ),
+                'filters':
+                {
+                    'Resource':
+                    [
+                        'mortorq',
+                        'frearson',
+                    ],
+                },
+            },
+            '33345',
+            'raw-data-every-1000-with-fields-and-filters.csv',
+        ),
+    ],
+)
+def test_get_raw_data(valid_dw, capsys, additional_params, number, csv_title):
+    defult_params = {
+        'duration': ('2016-01-01', '2016-12-31'),
+        'realm': 'Jobs',
+        'show_progress': True,
+    }
+    params = {**defult_params, **additional_params}
+    data = valid_dw.get_raw_data(**params).iloc[::1000]
     data.index = data.index.astype('string')
     __assert_dfs_equal(
-        'machine-learning-notebook-example-every-1000.csv',
+        csv_title,
         data,
         dtype='string',
         index_col=0,
     )
-    assert 'Got 42240 rows...DONE' in capsys.readouterr().out
+    assert 'Got ' + number + ' rows...DONE' in capsys.readouterr().out
 
 
 def __assert_descriptor_dfs_equal(data_file, actual):
@@ -101,8 +127,8 @@ def test_describe_dimensions(valid_dw):
 
 def test_get_filter_values(valid_dw):
     __assert_descriptor_dfs_equal(
-        'jobs-fieldofscience-filter-values.csv',
-        valid_dw.get_filter_values('Jobs', 'Field of Science'),
+        'jobs-pi-group-filter-values.csv',
+        valid_dw.get_filter_values('Jobs', 'PI Group'),
     )
 
 
@@ -110,7 +136,7 @@ def test_get_data_filter_user(valid_dw):
     # Make sure the filter validation works for a user whose list position is
     # greater than 10000 — this will raise an exception if it doesn't work.
     valid_dw.get_data(
-        duration=('2023-01-01', '2023-01-01'),
+        duration=('2016-01-01', '2017-12-31'),
         realm='Jobs',
         metric='CPU Hours: Total',
         dataset_type='aggregate',
@@ -119,17 +145,17 @@ def test_get_data_filter_user(valid_dw):
 
 
 @pytest.mark.parametrize(
-    'duration,aggregation_unit,data_file',
+    'aggregation_unit,data_file',
     [
-        (('2023-01-01', '2023-12-31'), 'Month', 'jobs-2023-months.csv'),
-        (('2023-01-01', '2023-12-31'), 'Quarter', 'jobs-2023-quarters.csv'),
-        (('2022-01-01', '2023-12-31'), 'Year', 'jobs-2022-2023-years.csv'),
+        ('Month', 'jobs-2016-2017-month.csv'),
+        ('Quarter', 'jobs-2016-2017-quarters.csv'),
+        ('Year', 'jobs-2016-2017-years.csv'),
     ],
     ids=('month', 'quarter', 'year'),
 )
-def test_get_data(valid_dw, duration, aggregation_unit, data_file):
+def test_get_data(valid_dw, aggregation_unit, data_file):
     data = valid_dw.get_data(
-        duration=duration,
+        duration=('2016-01-01', '2017-12-31'),
         realm='Jobs',
         metric='CPU Hours: Total',
         aggregation_unit=aggregation_unit,
